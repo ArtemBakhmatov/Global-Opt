ab5c6fef85f3a9590684abaff8a2455b611138fa
Artem <ip.bakhmatov@mail.ru>
2021-09-26 13:51:14 +0300

    Адаптация первого экрана, Продожение

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('click', function() {
+    let hamburger = document.querySelector('.hamburger'),
+        menu = document.querySelector('.header__menu'),
+        menuItem = document.querySelectorAll('.header__menu-item');
+
+    hamburger.addEventListener('click', function() {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header__menu_active');
+    });
+
+    menuItem.forEach(function(item) {
+        item.addEventListener('click', function() {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header__menu_active');
+        });
+    });
+});
